3ea7caea4a37324b94c46d15f8ad408a74728e93
David Gao <gao.ten@northeastern.edu>
2024-03-21 23:10:16 -0400

    add Movie object, analyze web source dom... need example how to extract :(

--- backend/fetch_movie.go
+++ backend/fetch_movie.go
@@ -1,59 +1,52 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gocolly/colly/v2"
-	"log"
-	"os"
-	"strings"
 )
-
-func main() {
-	// fetch movie info from IMDb
-	fetch_from_web()
-}
 
 // store movie info in a struct
 type Movie struct {
-	title  string
-	year   string
-	length unit32
-	genre  *string
-	stars  *string
+	title      string // lower case can only be accessed within the same package
+	year       string
+	length     uint32
+	rate_level string
+	review     float32
+	genre      *string
+	stars      *string
 }
 
 // get HTTP requests to IMDb's servers go get github.com/PuerkitoBio/goquery
 func fetch_from_web() {
+	c := colly.NewCollector()
 
-	// Create a new collector
-	c := colly.NewCollector(
-		// allow only domains: www.imdb.com
-		colly.AllowedDomains("imdb.com", "www.imdb.com"),
-		// set the recursion depth for links to visit, goes forever if not set
-		colly.MaxDepth(3),
-		// enables asynchronous network requests
-		colly.Async(true),
-	)
-	// visit only domains: www.imdb.com
+	movies := []Movie{}
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		log.Printf("Link found: %q -> %s\n", e.Text, link)
-		log.Printf("visiting", link)
-		// Visit link found on page
-		// Only those links are visited which are in the same domain
-		c.Visit(e.Request.AbsoluteURL(link))
+	c.OnHTML(".movie", func(e *colly.HTMLElement) {
+		fmt.Println("e: ", e)
+		movie := Movie{
+			title:      e.ChildText(".name"),
+			year:       e.ChildText(".release-date"),
+			length:     e.ChildText(".length"),
+			rate_level: e.ChildText(".rate-level"),
+			genre:      e.ChildText(".genre"),
+			stars:      e.ChildText(".stars"),
+		}
+		movies = append(movies, movie)
 	})
 
-	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://www.imdb.com
-	c.Visit(web_link)
+	c.Visit("http://example.com/movies")
 
+	for _, movie := range movies {
+		fmt.Printf("%+v\n", movie)
+	}
+}
+
+func main() {
+	// fetch movie info from IMDb
+	fetch_from_web()
 }
