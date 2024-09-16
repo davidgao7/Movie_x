82b48fdf6bcf524834d81e7babb686d84a1ba221
David Gao <gao.ten@northeastern.edu>
2024-09-16 19:31:55 -0400

    faster reading tsv

--- backend/fetch_movie.go
+++ backend/fetch_movie.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"fmt"
-	"log"
 	"os"
+	"sync"
 	// "io"
 	// "net/http"
-	"reflect"
-	"strings"
 )
 
 // store movie info in a struct
@@ -23,68 +20,50 @@ type Movie struct {
 	stars      *string
 }
 
-// TODO: implement multi threading for fast fetching later
-func fetch_from_csv() {
-	file, err := os.Open("../data/title.basics.tsv")
+// implement multi threading for fast fetching
+func fetch_from_csv(filePath string, numNumWorkers int) {
+	// open the file
+	file, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer file.Close()
 
+	// create channels for worker communication
+	linesChan := make(chan string, 100) // buffer size 100
+	var wg sync.WaitGroup               // wait for all workers to finish
+
+	// start workers goroutines
+	for i := 0; i < numNumWorkers; i++ {
+		wg.Add(1)
+		go processLine(linesChan, &wg)
+	}
+
+	// read file line by line and send to workers
 	scanner := bufio.NewScanner(file)
-	csvReader := csv.NewReader(nil)
-
-	// Read the first line (column names)
-	if scanner.Scan() {
-		csvReader = csv.NewReader(strings.NewReader(scanner.Text()))
-		csvReader.Comma = '\t'
-		columnNames, err := csvReader.Read()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Column names:", columnNames)
-		fmt.Println("type of columnNames: ", reflect.TypeOf(columnNames[0]))
-	}
-
-	// Read the rest of the file
 	for scanner.Scan() {
-		csvReader = csv.NewReader(strings.NewReader(scanner.Text()))
-		csvReader.Comma = '\t'
-		record, err := csvReader.Read()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("=====================")
-		fmt.Println(record)
-		fmt.Println(record[0])
-		fmt.Println("type of record[0]: ", reflect.TypeOf(record[0]))
-		fmt.Println("=====================")
-		// title      string // lower case can only be accessed within the same package
-		// year       string
-		// length     uint32
-		// rate_level string
-		// review     float32
-		// genre      *string
-		// stars      *string
-
-		title := record[2]      // primaryTitle in csv
-		year := record[5]       // startYear in csv
-		length := record[7]     // runtimeMinutes in csv
-		rate_level := record[8] // averageRating in csv
-		review := record[9]     // numVotes in csv
-		genre := record[-1]     // genres in csv
-
-		// TODO: deal with one line first
-		break
+		linesChan <- scanner.Text()
 	}
+	close(linesChan) // no more lines to read
+
+	// wait for all workers to finish
+	wg.Wait()
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+}
 
+func processLine(linesChan chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for line := range linesChan {
+		fmt.Println(line)
+		os.Exit(0)
+
+	}
 }
 
 func main() {
 	// fetch movie info from IMDb
-	fetch_from_csv()
+	fetch_from_csv("../data/title.basics.tsv", 10)
 }
